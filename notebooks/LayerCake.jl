3c509ddae8fe447740121302834ef6c248900306
cdacosta <c.dacostaf@gmail.com>
2018-03-31 20:27:09 -0300

    minor details

--- notebooks/LayerCake.jl
+++ notebooks/LayerCake.jl
@@ -13,23 +13,23 @@ rho[div(670,dz):end,:] -= 500
 
 vel = similar(rho); vel .= 2000;
 
-blk = rho.*vel.^2;
+blk = 1e-9rho.*vel.^2;
 
 fig, ax = plt.subplots(1, 3, figsize=(12,3))
 
 # Density
-cax = ax[1][:imshow](rho, extent=[x[1], x[end], z[end], z[1]], aspect="equal", cmap="Blues")
+cax = ax[1][:imshow](rho, extent=[x[1], x[end], z[end], z[1]], aspect="equal", cmap="Blues", interpolation="none")
 ax[1][:set](xlabel="Position [m]", ylabel="Depth [m]")
 cbar = fig[:colorbar](cax, ax=ax[1])
 cbar[:ax][:set](ylabel="Density [kg/m³]")
 
 # Velocity
-cax = ax[2][:imshow](vel, extent=[x[1], x[end], z[end], z[1]], aspect="equal", cmap="Blues")
+cax = ax[2][:imshow](vel, extent=[x[1], x[end], z[end], z[1]], aspect="equal", cmap="Blues", interpolation="none")
 ax[2][:set](xlabel="Position [m]", yticks=[])
 cbar = fig[:colorbar](cax, ax=ax[2])
 cbar[:ax][:set](ylabel="Velocity [m/s]");
 
-cax = ax[3][:imshow](1e-9blk, extent=[x[1], x[end], z[end], z[1]], aspect="equal", cmap="Blues")
+cax = ax[3][:imshow](blk, extent=[x[1], x[end], z[end], z[1]], aspect="equal", cmap="Blues", interpolation="none")
 ax[3][:set](xlabel="Position [m]", yticks=[])
 cbar = fig[:colorbar](cax, ax=ax[3])
 cbar[:ax][:set](ylabel="Bulk modulus [GPa]");
@@ -76,15 +76,15 @@ con = ax[:contour](trav[:,:,div(ns,3)], extent=[x[1], x[end], z[end], z[1]], ori
 plt.clabel(con, fontsize=9, inline=1, fmt="%1.2fs");
 ax[:set](ylabel="Traveltime [s]", xlim=(x[1], x[end]), ylim=(z[end], z[1]));
 
-t = 0:0.008:1; nt = length(t); reload("KirchMig")
-L = KirchMig.KirchMap(t, trav)
+t = 0:0.008:1; nt = length(t)
+L = km.KirchMap(t, trav)
 
 @time d = L*mod_bl[:];
 
 data = reshape(d, ns, ns, nt)
 
 rows = 10
-fig, ax = plt.subplots(rows, div(ns, rows), figsize=(12,4*rows))
+fig, ax = plt.subplots(rows, div(ns, rows), figsize=(12,4rows))
 idx = reshape(1:rows*div(ns, rows), div(ns, rows), rows)'
 for (i, axi) in enumerate(ax)
     csg = data[:,idx[i],:]'
@@ -103,7 +103,7 @@ fig[:tight_layout]()
 
 @time m_mig = L'd;
 
-xran = src_x[end]-src_x[1]
+xran = x[end]-x[1]
 mod_mig = reshape(m_mig, nz, nx)
 vmin, vmax = maximum(abs.(mod_mig))*[-1,1]
 fig, ax = plt.subplots(figsize=(6,4))
@@ -116,7 +116,7 @@ ax[:set](xlim=(x[1], x[end]), ylim=(z[end], z[1]), xlabel="Position [m]", ylabel
     title="Migrated image")
 fig[:tight_layout]()
 
-@time m_lsm, _, hist_r = KirchMig.cg(L'L, m_mig, maxiter=20, log=true);
+@time m_lsm, _, hist_r = km.cg(L'L, m_mig, maxiter=20, log=true);
 
 mod_lsm = reshape(m_lsm, nz, nx)
 vmin, vmax = maximum(abs.(mod_lsm))*[-1,1]
@@ -144,14 +144,14 @@ err_lsm = mod_bl/maximum(mod_bl) - mod_lsm/maximum(mod_lsm)
 fig, ax = plt.subplots(1, 2, figsize=(12,4))
 pclip=.5
 cax = ax[1][:imshow](err_mig, extent=[x[1], x[end], z[end], z[1]],
-    vmin=-pclip, vmax=pclip, aspect="equal", cmap="gray")
+    vmin=-pclip, vmax=pclip, aspect="equal", cmap="gray", interpolation="none")
 ax[1][:set](xlabel="Position [m]", ylabel="Depth [m]", title="Normalized migration error")
 cbar = fig[:colorbar](cax, ax=ax[1])
 cbar[:ax][:set](ylabel="Amplitude");
 
 vmin, vmax = pclip*maximum(abs.(mod_lsm))*[-1,1]
 cax = ax[2][:imshow](err_lsm, extent=[x[1], x[end], z[end], z[1]],
-    vmin=-pclip, vmax=pclip, aspect="equal", cmap="gray")
+    vmin=-pclip, vmax=pclip, aspect="equal", cmap="gray", interpolation="none")
 ax[2][:set](xlabel="Position [m]",title="Normalized LSM error", yticks=[]);
 cbar = fig[:colorbar](cax, ax=ax[2])
 cbar[:ax][:set](ylabel="Amplitude");
