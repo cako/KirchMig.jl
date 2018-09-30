e1f03d8631b6290b8758898bdea2f4a102804713
Carlos da Costa <cako@users.noreply.github.com>
2018-09-30 19:27:29 -0300

    0.7

--- docs/make.jl
+++ docs/make.jl
@@ -8,6 +8,6 @@ makedocs(
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/cako/KirchMig.jl",
-    julia  = "0.6",
+    julia  = "0.7",
     osname = "linux"
 )
