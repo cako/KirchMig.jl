d37395cf1c43a20e870584b2b470db6622e58513
Carlos <c.dacostaf+github@gmail.com>
2019-04-07 20:56:21 -0300

    update docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(
 )
  
 deploydocs(
-    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    deps   = Deps.pip("mkdocs", "mkdocs-material", "python-markdown-math"),
     repo = "github.com/cako/KirchMig.jl",
     julia  = "1.0",
     osname = "linux"
